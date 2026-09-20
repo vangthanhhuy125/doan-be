9e81cb4e4d2d64a46685b2329551843a3de71f0f
vangthanhhuy125 <vangthanhhhuy010205@gmail.com>
2026-09-20 22:26:57 +0700

    temp: get env

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -9,4 +9,18 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('lay-lai-env-bi-mat')
+  getEnvBiMat() {
+    return {
+      DB_NAME: process.env.DB_NAME,
+      JWT_SECRET: process.env.JWT_SECRET,
+      JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN,
+      MAIL_USER: process.env.MAIL_USER,
+      MAIL_PASS: process.env.MAIL_PASS,
+      PORT: process.env.PORT,
+      FRONTEND_URL: process.env.FRONTEND_URL,
+      MONGODB_URI: process.env.MONGODB_URI,
+    };
+  }
 }
